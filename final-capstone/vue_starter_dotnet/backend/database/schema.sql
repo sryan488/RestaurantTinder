eeed22666866d7a6c08c5838c912f85ed720e989
Sean Ryan <sryan488@gmail.com>
2019-12-09 15:36:54 -0500

    SQL jazz

--- final-capstone/vue_starter_dotnet/backend/database/schema.sql
+++ final-capstone/vue_starter_dotnet/backend/database/schema.sql
@@ -29,5 +29,39 @@ CREATE TABLE users
 	constraint pk_users primary key (id)
 );
 
+select * from users
 
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE User_Preferences
+(
+	users_id int,
+	city varchar(50) not null,
+	cuisine varchar(50) not null,
+	price int not null,
+	distance int not null
+
+);
+
+
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE Favorites
+(
+	users_id	int,
+	restaurant_id int,
+);
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE Blacklist
+(
+	users_id	int,
+	restaurant_id int,
+);
 COMMIT TRANSACTION;
