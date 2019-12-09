810c47ffc2d1cd67599b4604bc12151ad3d9442b
Joe LaBianca <jalabianca@gmail.com>
2019-12-09 14:19:06 -0500

    pushing rough form vue

--- final-capstone/vue_starter_dotnet/frontend/src/router.js
+++ final-capstone/vue_starter_dotnet/frontend/src/router.js
@@ -4,6 +4,7 @@ import auth from './auth'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Register from './views/Register.vue'
+import PreferenceForm from './views.PreferenceForm.vue'
 
 Vue.use(Router)
 
@@ -44,21 +45,30 @@ const router = new Router({
         requiresAuth: false
       }
     },
+    {
+      path: '/preferenceForm',
+      name: 'preferenceForm',
+      component: PreferenceForm,
+      meta: {
+        requiresAuth: false
+      }
+    },
+
   ]
 })
 
-router.beforeEach((to, from, next) => {
-  // Determine if the route requires Authentication
-  const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
-  const user = auth.getUser();
+// router.beforeEach((to, from, next) => {
+//   // Determine if the route requires Authentication
+//   const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
+//   const user = auth.getUser();
 
-  // If it does and they are not logged in, send the user to "/login"
-  if (requiresAuth && !user) {
-    next("/login");
-  } else {
-    // Else let them go to their next destination
-    next();
-  }
-});
+//   // If it does and they are not logged in, send the user to "/login"
+//   if (requiresAuth && !user) {
+//     next("/login");
+//   } else {
+//     // Else let them go to their next destination
+//     next();
+//   }
+// });
 
 export default router;
---
+++ final-capstone/vue_starter_dotnet/frontend/src/views/PreferenceForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+    <h1> FILL OUT THIS FORM </h1>
+        <form v-on:submit.prevent="submitPreferences">
+            <div>
+                <label for=cuisine>Name:</label><input id="cuisine" type="text" v-model="preferencesForm.cuisine" />
+            </div>
+            <div>
+                <label for=price>Name:</label><input id="price" type="number" v-model="preferencesForm.price" />
+            </div>
+            <div>
+                <label for=location>Name:</label><input id="location" type="text" v-model="preferencesForm.location" />
+            </div>
+            <div>
+                <label for=distance>Name:</label><input id="distance" type="number" v-model="preferencesForm.distance" />
+            </div>
+
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'preferencesForm',
+  data() {
+      return {
+          cuisine: "",
+          price: 0,
+          location: "",
+          distance: 0
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
