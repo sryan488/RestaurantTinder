4993bb8c819dac7ae9add91e4671a83d0e86ce22
Joe LaBianca <jalabianca@gmail.com>
2019-12-09 17:54:00 -0500

    still doing the form

--- final-capstone/vue_starter_dotnet/frontend/src/views/PreferenceForm.vue
+++ final-capstone/vue_starter_dotnet/frontend/src/views/PreferenceForm.vue
@@ -4,33 +4,33 @@
         <form v-on:submit.prevent="submitPreferences">
             <div>
                 <h2> Cuisine </h2>
-                <label for=italian>Italian </label>
-                <input type="checkbox" >
-                <label for=cuisine>Chinese </label>
-                <input type="checkbox" >
-                <label for=cuisine>Mexican </label>
-                <input type="checkbox" >
-                <label for=cuisine>American </label>
-                <input type="checkbox" >
+                    <label for=italian>Italian </label>
+                        <input type="checkbox" v-model="form.cuisine">
+                    <label for=cuisine>Chinese </label>
+                        <input type="checkbox" v-model="form.cuisine">
+                    <label for=cuisine>Mexican </label>
+                        <input type="checkbox" v-model="form.cuisine">
+                    <label for=cuisine>American </label>
+                        <input type="checkbox" v-model="form.cuisine">
                 <h2> Price Range </h2>
-                <label for=$>$ </label>
-                <input type="checkbox" >
-                <label for=$$>$$ </label>
-                <input type="checkbox" >
-                <label for=$$$>$$$ </label>
-                <input type="checkbox" >
-                <label for=$$$$>$$$$ </label>
-                <input type="checkbox" >
+                    <label for=$>$ </label>
+                        <input type="checkbox" v-model="form.priceRange">
+                    <label for=$$>$$ </label>
+                        <input type="checkbox" v-model="form.priceRange">
+                    <label for=$$$>$$$ </label>
+                        <input type="checkbox" v-model="form.priceRange">
+                    <label for=$$$$>$$$$ </label>
+                        <input type="checkbox" v-model="form.priceRange">
                 <h2> Location </h2>
-                <label for=city>City: </label>
-                <input type="text" >
+                    <label for=city>City: </label>
+                        <input type="text" v-model="form.city">
                 <h2> Distance 
-                    <select>
-                        <option value="5 mi">5 mi</option>
-                        <option value="10 mi">10 mi</option>
-                        <option value="25 mi">25 mi</option>
-                        <option value="50 mi">50 mi</option>
-                        <option value="100 mi">100 mi</option>
+                    <select v-model="form.city">
+                        <option value="5" >5 mi</option>
+                        <option value="10">10 mi</option>
+                        <option value="25">25 mi</option>
+                        <option value="50">50 mi</option>
+                        <option value="100">100 mi</option>
                     </select>
                 </h2>
             </div>
@@ -43,10 +43,12 @@ export default {
   name: 'preferencesForm',
   data() {
       return {
+          form: {
           cuisine: [],
           priceRange: [],
           city: "",
           searchRadius: 0
+          }
       }
   }
 }
