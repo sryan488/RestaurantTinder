655b403611c3571be2c8d06f29db3afe83e5a908
Steve Roy <scr44@case.edu>
2019-12-09 13:28:13 -0500

    Added preferences class.

---
+++ final-capstone/vue_starter_dotnet/backend/SampleApi/Models/Preferences.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.Models
+{
+    public class Preferences
+    {
+        public Preferences()
+        {
+
+        }
+        public int UserID { get; set; }
+
+        public List<string> Categories { get; set; }
+
+        public int PriceRange { get; set; }
+
+        public string City { get; set; }
+
+        public double Distance { get; set; } // distance in miles
+
+    }
+}
