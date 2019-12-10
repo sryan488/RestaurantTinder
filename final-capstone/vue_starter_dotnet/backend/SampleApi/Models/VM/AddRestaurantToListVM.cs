15b81ba301a1781cfa62d68627bbedcb59af147f
Alex Frankel <alex@argoblue.com>
2019-12-10 11:06:55 -0500

    added VM

---
+++ final-capstone/vue_starter_dotnet/backend/SampleApi/Models/VM/AddRestaurantToListVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.Models.VM
+{
+    public class AddRestaurantToListVM
+    {
+        /// <summary>
+        /// The restaurant id.
+        /// </summary>
+        public int RestaurantID { get; set; }
+
+        /// <summary>
+        /// The user's id.
+        /// </summary>        
+        public int Id { get; set; }
+
+        /// <summary>
+        /// True for fav or false for blacklist
+        /// </summary>
+        public Boolean favOrBlack { get; set; }
+
+        public User User { get; set; }
+
+        public Restaurant Restaurant { get; set; }
+    }
+}
