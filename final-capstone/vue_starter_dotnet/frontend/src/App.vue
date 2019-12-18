3f5617af8831edb361a44effaa09962f7519235d
Alex Frankel <alex@argoblue.com>
2019-12-18 12:49:28 -0500

    w

--- final-capstone/vue_starter_dotnet/frontend/src/App.vue
+++ final-capstone/vue_starter_dotnet/frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#"><i class="fas fa-hamburger"></i> Wild Card</a>
+  <a class="navbar-brand" href="#"><i class="fas fa-hamburger fa-spin-hover"></i> Wild Card</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     
     <span class="navbar-toggler-icon"></span>
